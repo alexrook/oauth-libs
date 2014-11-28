826fb518dae527f93040942eafefd9f64ba2dd06
Alex Rook <rook.alex@gmail.com>
2014-11-28 14:26:31 +0200

    small setup fixes

---
+++ oauth-learn-wi/src/main/java/oul/rest/App.java
@@ -0,0 +1,13 @@
+package oul.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author moroz
+ */
+@ApplicationPath("/rest")
+public class App extends Application {
+
+
+}
