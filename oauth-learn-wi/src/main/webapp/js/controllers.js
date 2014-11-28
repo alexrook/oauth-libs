fe4facb728ae0824813bc8567e7b8450379ef79f
Alex Rook <rook.alex@gmail.com>
2014-11-28 14:05:11 +0200

    add initial mavenized project structure seeded from angularjs-sample

---
+++ oauth-learn-wi/src/main/webapp/js/controllers.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('oulApp.controllers', [])
+  .controller('MyCtrl1', ['$scope', function($scope) {
+
+  }])
+  .controller('MyCtrl2', ['$scope', function($scope) {
+
+  }]);
