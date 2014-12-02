44cc03d7b8c5e26eb99778489c19af15d4a4dd70
alexrook <rook.alex@gmail.com>
2014-12-02 20:02:22 +0200

    test google authsub

--- oauth-learn-wi/src/main/webapp/js/app.js
+++ oauth-learn-wi/src/main/webapp/js/app.js
@@ -6,7 +6,7 @@ angular.module('oulApp', [
     'oulApp.services',
     'oulApp.controllers'
 ]).config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+        $routeProvider.when('/authsub', {templateUrl: 'partials/authsub.html', controller: 'AuthSubCtrl'});
         $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-        $routeProvider.otherwise({redirectTo: '/view1'});
+        $routeProvider.otherwise({redirectTo: '/authsub'});
     }]);
--- oauth-learn-wi/src/main/webapp/js/controllers.js
+++ oauth-learn-wi/src/main/webapp/js/controllers.js
@@ -3,9 +3,16 @@
 /* Controllers */
 
 angular.module('oulApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+        .controller('AuthSubCtrl', ['$scope', '$routeParams', function ($scope, $routeParams) {
+            console.log($routeParams);
+               // console.log($location.absUrl());
+               // console.log($location.hash());
+            }])
+        .controller('MyCtrl2', ['$scope', function ($scope) {
 
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
-
-  }]);
+            }])
+        .controller('MainCtrl', ['$scope', '$location', function ($scope, $location) {
+                console.log($location.search());
+                console.log($location.absUrl());
+                console.log($location.hash());
+            }]);
