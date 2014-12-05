f08c533e24a1990b7efa5e8c28d4d79270e953e0
Alex <rook.alex@gmail.com>
2014-12-05 15:10:19 +0200

    add auth proxy service

---
+++ oauth-learn-wi/src/main/java/oul/rest/tools/ProxyRS.java
@@ -0,0 +1,95 @@
+package oul.rest.tools;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * @author moroz
+ */
+@Path("/proxy")
+public class ProxyRS {
+
+    private static final String X_PROXY_HEADER = "X-Proxy-This";
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response proxy(@Context HttpHeaders headers, @QueryParam("uri") String uri) {
+        int responseCode = 0;
+        try {
+            URL url = new URL(uri);
+            HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+            httpConn.setRequestMethod("GET");
+
+            Map<String, String> proxyHeaders = getProxyHeaders(headers);
+            for (String k : proxyHeaders.keySet()) {
+                httpConn.setRequestProperty(k, proxyHeaders.get(k));
+            }
+            responseCode = httpConn.getResponseCode();
+            if (responseCode > 400) {
+                throw new IOException(httpConn.getResponseMessage());
+            }
+
+            StringBuilder buf = new StringBuilder();
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(httpConn.getInputStream()));
+            String line = reader.readLine();
+            while (line != null) {
+                buf.append(line);
+                line = reader.readLine();
+            }
+
+            httpConn.disconnect();
+
+            return Response.ok(buf.toString()).build();
+
+        } catch (Exception e) {
+            return Response
+                    .status(Response.Status.BAD_REQUEST)
+                    .entity(responseCode + ":" + e.getMessage()).build();
+        }
+
+    }
+
+    private Map<String, String> getProxyHeaders(HttpHeaders headers) throws IOException {
+
+        Map<String, String> map = new HashMap<String, String>();
+
+        for (String key : headers.getRequestHeaders().keySet()) {
+            if (key.toLowerCase().startsWith(X_PROXY_HEADER.toLowerCase())) {
+                StringBuilder buf = new StringBuilder();
+                String hk = "";
+                int i = 0;
+                for (String v : headers.getRequestHeader(key)) {
+                    if (i == 0) {
+                        int k = v.indexOf("=");
+                        if (k < 0) {
+                            throw new IOException("malformed X-Proxy-This header");
+                        }
+                        hk = v.substring(0, k);
+                        buf.append(v.substring(k + 1));
+                    } else {
+                        buf.append(v);
+                    }
+                    i++;
+
+                }
+                map.put(hk, buf.toString());
+            }
+
+        }
+        return map;
+    }
+}
--- oauth-learn-wi/src/main/webapp/js/controllers.js
+++ oauth-learn-wi/src/main/webapp/js/controllers.js
@@ -3,39 +3,77 @@
 /* Controllers */
 
 function getNativeSearchObj() {
-    var result={};
-    
+    var result = {};
+
     if (window.location.search) {
-	var rsearch=window.location.search.substring(1,window.location.search.length);
-	
-	angular.forEach(rsearch.split('&'),function(pair){
-		pair=pair.split('=');
-		if (pair.length>1) {
-		    result[pair[0]]=pair[1];
-		}else
-		    result[pair[0]]=true;
-	    });
-	
+
+        var rsearch = decodeURIComponent(
+                window.location.search.substring(1, window.location.search.length));
+
+        angular.forEach(rsearch.split('&'), function (pair) {
+            pair = pair.split('=');
+            if (pair.length > 1) {
+                result[pair[0]] = pair[1];
+            } else
+                result[pair[0]] = true;
+        });
+
     }
-    
+
     return result;
 }
 
 angular.module('oulApp.controllers', [])
-        .controller('AuthSubCtrl', ['$scope', '$routeParams', function ($scope, $routeParams) {
-		$scope.authSub2Call=function(){
-		    
-		}
+        .controller('AuthSubCtrl', ['$scope', '$http', function ($scope, $http) {
+
+                $scope.STATES = {FIRST: 1, SECOND: 2, THREE: 3, ERROR: -1};
+                $scope.state = $scope.STATES.FIRST;
+                $scope.authsub = {
+                    slsuToken: (getNativeSearchObj().token ? getNativeSearchObj().token : '')
+                };
+                if ($scope.authsub.slsuToken) {
+                    $scope.state = $scope.STATES.SECOND;
+                }
+
+                $scope.authSub2Call = function () {
+
+                    var req = {
+                        method: 'GET',
+                        url: 'http://devhelper:8080/oul/rest/proxy',
+                        params: {
+                            uri: 'https://google.com/accounts/AuthSubSessionToken'
+                        },
+                        headers: {
+                            'X-Proxy-This': 'Authorization=' + 'AuthSub token=' + $scope.authsub.slsuToken
+                        }
+
+                    };
+
+                    $http(req)
+                            .success(function (data, status) {
+                                console.log(data);
+                                $scope.sessionToken = data;
+                                $scope.state = $scope.STATES.THREE;
+                            })
+                            .error(function (data, status) {
+                                console.log(data);
+                                $scope.err = {
+                                    data: data,
+                                    status: status
+                                };
+                                $scope.state = $scope.STATES.ERROR;
+                            });
+                };
             }])
         .controller('MyCtrl2', ['$scope', function ($scope) {
 
             }])
-        .controller('MainCtrl', ['$scope','$location',
-		function ($scope, $location) {
-                $scope.host=$location.host();
-		$scope.port=$location.port();
-		$scope.search=angular.toJson($location.search());
-		$scope.searchNative=angular.toJson(getNativeSearchObj());
-		$scope.hash=$location.hash();
-		$scope.path=$location.path();
+        .controller('MainCtrl', ['$scope', '$location',
+            function ($scope, $location) {
+                $scope.host = $location.host();
+                $scope.port = $location.port();
+                $scope.search = angular.toJson($location.search());
+                $scope.searchNative = angular.toJson(getNativeSearchObj());
+                $scope.hash = $location.hash();
+                $scope.path = $location.path();
             }]);
