55aecb5e07c75a53270c01116a07e82582043d5f
Alex Rook <rook.alex@gmail.com>
2014-12-26 12:25:14 +0200

    add MemoryUserStorage, not tested

---
+++ oauth-learn-wi/src/main/java/oul/rest/tools/oauth/IConst.java
@@ -0,0 +1,10 @@
+package oul.rest.tools.oauth;
+
+/**
+ * @author moroz
+ */
+public interface IConst {
+
+    public static String AUTH_COOKIE_NAME = "AUTH_ID";
+
+}
---
+++ oauth-learn-wi/src/main/java/oul/rest/tools/oauth/IUserStorageEx.java
@@ -0,0 +1,20 @@
+package oul.rest.tools.oauth;
+
+import java.io.IOException;
+import javax.servlet.http.Cookie;
+
+/**
+ * @author moroz
+ */
+public interface IUserStorageEx extends IUserStorage {
+
+    Cookie registerEx(Profile profile)
+            throws IOException;
+
+    Profile get(Cookie cookie) throws IOException;
+
+    boolean check(Cookie cookie) throws IOException;
+
+    boolean unregister(Cookie cookie) throws IOException;
+
+}
